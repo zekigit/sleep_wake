8ba3ef4b3a9b767e3cb211eab35255f2ea96ff0f
Ezequiel <e.mikulan@gmail.com>
2016-11-01 19:09:59 -0300

    mi

--- scripts/ita_info.py
+++
@@ -1,8 +0,0 @@
-conditions = ['wake', 'sleep']
-
-bad_ch_eeg = {
-    'wake': ['31', '45', '93', '132', '176',  '219'],
-    'sleep': []}
-
-results_path = '/Users/lpen/PycharmProjects/Wake_Sleep/milano/results/'
-figures_path = '/Users/lpen/PycharmProjects/Wake_Sleep/milano/figures/'
---
+++ scripts/mi_info.py
@@ -0,0 +1,8 @@
+conditions = ['wake', 'sleep']
+
+bad_ch_eeg = {
+    'wake': ['31', '45', '93', '132', '176',  '219'],
+    'sleep': []}
+
+results_path = '/Users/lpen/PycharmProjects/Wake_Sleep/mi/results/'
+figures_path = '/Users/lpen/PycharmProjects/Wake_Sleep/mi/figures/'
